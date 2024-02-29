9e90ebd51603d3900678d7091e4ad3743ab8b7fb
Gerald Susanteo <66259062+geraldsusanteo@users.noreply.github.com>
2024-02-28 22:00:45 -0800

    Update Main.java

--- HW/hw8/Main.java
+++ HW/hw8/Main.java
@@ -1,3 +1,38 @@
+import java.util.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        String s = "abccccdd"; // 2nd input case = "speediskey" output = 5
+        if (s.length() >= 1 && s.length() <= 2000) {
+            System.out.println(longestPalindrome(s));
+        } else {
+            System.out.println("Length Invalid");
+        }
+    }
+
+    public static int longestPalindrome(String s) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        boolean oddFound = false;
+        int maxLength = 0;
+
+        for (int count : charCount.values()) {
+            if (count % 2 == 0) {
+                maxLength += count;
+            } else {
+                maxLength += count - 1;
+                oddFound = true;
+            }
+        }
+
+        if (oddFound) {
+            maxLength++; // Includes one odd count character
+        }
+
+        return maxLength;
+    }
 }
+
